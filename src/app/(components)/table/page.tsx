569e5ca5b99e7bc2a7e2507f1b56b1fe3834b776
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2024-09-04 02:01:52 -0500

    feat: Add table component

--- src/app/(components)/table/page.tsx
+++ src/app/(components)/table/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <div>page</div>
+import { taskSchema } from "./data/schema"
+import { DataTable } from "./table/data-table"
+import path from "path"
+import { z } from "zod"
+import { promises as fs } from "fs"
+import { columns } from "./table/columns"
+
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/app/(components)/table/data/task.json")
+  )
+
+  const tasks = JSON.parse(data.toString())
+
+  return z.array(taskSchema).parse(tasks)
+}
+
+
+export default async function DemoPage() {
+  const tasks = await getTasks()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={tasks} />
+    </div>
+  )
 }
--- src/app/(portal)/page.tsx
+++ src/app/(portal)/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
+import DemoPage from '../(components)/table/page'
 
 export default function Home() {
-  return <div></div>
+  return <div className='container max-w-screen-2xl flex flex-col gap-7 justify-center items-center py-6'>
+    <h1>Home</h1>
+    <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    <DemoPage />
+  </div>
 }
