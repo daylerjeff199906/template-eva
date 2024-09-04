d8f6594a410054c0bc896d548735ce9a32497a30
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-09-04 08:00:51 -0500

    feat: Update HeaderSection component to include renderLeftSection and renderRightSection props

--- src/modules/core/components/HeaderSection/HeaderSection.tsx
+++ src/modules/core/components/HeaderSection/HeaderSection.tsx
@@ -7,11 +7,21 @@ interface IHeaderSectionProps {
   showBackButton?: boolean
   disableAddButton?: boolean
   labelAddButton?: string
+  renderLeftSection?: React.ReactNode
+  renderRightSection?: React.ReactNode
 }
 
 export const HeaderSection = (props: IHeaderSectionProps) => {
-  const { showBackButton, disableAddButton, subtitle, title, labelAddButton } =
-    props
+  const {
+    showBackButton,
+    disableAddButton,
+    subtitle,
+    title,
+    labelAddButton,
+    renderLeftSection,
+    renderRightSection,
+  } = props
+
   return (
     <main className="py-6 border-b">
       <section className="container flex justify-between items-center">
@@ -27,10 +37,19 @@ export const HeaderSection = (props: IHeaderSectionProps) => {
             </h1>
             <h2 className="text-sm text-gray-500">{subtitle || 'Subtitle'}</h2>
           </section>
-          <section id="left-section"></section>
+          {renderLeftSection && (
+            <section
+              id="left-section"
+              className=""
+            >
+              {renderLeftSection}
+            </section>
+          )}
         </div>
         <div className="flex items-center gap-4">
-          <section id="right-section"></section>
+          {renderRightSection && (
+            <section id="right-section">{renderRightSection}</section>
+          )}
           {!disableAddButton && (
             <Button>
               <div className="flex gap-4 items-center">
