77d36d0997aac74434eadd6cca42a264416dc240
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-09-04 10:28:45 -0500

    feat: Add menu navigation to Layout component
    The commit adds menu navigation functionality to the Layout component in the panel section. The menu items are dynamically rendered based on the user's role (director or teacher).

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -1,7 +1,88 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import { NavBarCustom } from '@/modules/core'
+import { MenuItems } from '@/types'
+
 interface LayoutProps {
   children: React.ReactNode
 }
+
+const menuNav: MenuItems.IMenuItem[] = [
+  {
+    label: 'FeedBack',
+    href: '/feedback',
+  },
+  {
+    label: 'Historial',
+    href: '/about',
+  },
+  {
+    label: 'Ayuda',
+    href: '/help',
+  },
+]
+
+const menuItemsTeacher: MenuItems.IMenuItem[] = [
+  {
+    label: 'Inicio',
+    href: '/teacher',
+  },
+  {
+    label: 'Evaluaciones',
+    href: '/teacher/evaluations',
+  },
+  {
+    label: 'Perfil',
+    href: '/teacher/profile',
+  },
+]
+
+const menuItemsDirector: MenuItems.IMenuItem[] = [
+  {
+    label: 'Inicio',
+    href: '/director',
+  },
+  {
+    label: 'Gestionar aulas',
+    href: '/director/manage-classrooms',
+  },
+  {
+    label: 'Gestionar docentes',
+    href: '/director/manage-teachers',
+  },
+  {
+    label: 'Evaluaciones',
+    href: '/director/evaluations',
+  },
+  {
+    label: 'Asignar evaluaciones',
+    href: '/director/evaluations/assign-evaluations',
+  },
+  {
+    label: 'Perfil',
+    href: '/director/profile',
+  },
+]
+
 export default function Layout(props: LayoutProps) {
   const { children } = props
-  return <main>{children}</main>
+  const pathname = usePathname()
+  const initialPath = pathname.split('/')[1]
+
+  const menuItems =
+    initialPath === 'teacher' ? menuItemsTeacher : menuItemsDirector
+
+  return (
+    <main>
+      <NavBarCustom
+        menuNavbar={menuNav}
+        menuFooter={menuItems}
+        user={{
+          name: 'Pancracio López',
+          email: 'pancracio@gmail.com',
+          roles: [{ label: 'Director', href: '/director' }],
+        }}
+      />
+    </main>
+  )
 }
