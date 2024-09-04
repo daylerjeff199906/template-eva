1524fe54bb4bffb17757fe1940d0594af0e7b6bd
daylerjeff199906 <daylersan@gmail.com>
2024-09-03 21:00:47 -0500

    add componnet user dropdown

--- src/modules/core/components/NavBarCustom/NavBarCustom.tsx
+++ src/modules/core/components/NavBarCustom/NavBarCustom.tsx
@@ -8,6 +8,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import Link from 'next/link'
 
 const options = [
   { value: '1', label: 'Option 1' },
@@ -15,10 +16,27 @@ const options = [
   { value: '3', label: 'Option 3' },
 ]
 
-export const NavBarCustom = () => {
+const menuNavbar = [
+  { label: 'Feddback', href: '/feedback' },
+  { label: 'Historial', href: '/history' },
+  { label: 'ayuda', href: '/help' },
+]
+
+const menuFooter = [
+  { label: 'Terminos y condiciones', href: '/terms' },
+  { label: 'Politicas de privacidad', href: '/privacy' },
+  { label: 'Contacto', href: '/contact' },
+]
+
+interface IProps {
+  disabledItemsFoote?: boolean
+}
+
+export const NavBarCustom = (props: IProps) => {
+  const { disabledItemsFoote } = props
   return (
-    <nav>
-      <main className="container flex justify-between">
+    <nav className="flex flex-col">
+      <main className="container flex justify-between py-2 items-center">
         <section className="flex gap-4 items-center">
           <div
             id="section-brand"
@@ -57,14 +75,34 @@ export const NavBarCustom = () => {
           </div>
         </section>
         <section className="flex gap-6">
-          <div className="flex">
-            <a href="#">Home</a>
-            <a href="#">Home</a>
-            <a href="#">Home</a>
+          <div className="flex gap-2 items-center">
+            {menuNavbar.map((item) => (
+              <Link
+                key={item.label}
+                href={item.href}
+                className="border py-2 px-3 rounded-md text-xs font-medium"
+              >
+                {item.label}
+              </Link>
+            ))}
           </div>
+          <hr className="h-10 w-0.5 bg-gray-300" />
           <div></div>
         </section>
       </main>
+      {!disabledItemsFoote && (
+        <footer className="flex border-b">
+          {menuFooter.map((item) => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className="border-b py-2 px-3 rounded-md text-xs font-semibold"
+            >
+              {item.label}
+            </Link>
+          ))}
+        </footer>
+      )}
     </nav>
   )
 }
---
+++ src/modules/core/components/UserDropdown/UserDropdown.tsx
@@ -0,0 +1,3 @@
+export const UserDropdown = () => {
+  return <div>UserDropdown</div>
+}
