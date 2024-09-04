10fb56d74f5397436cebd1d36fb6b20555e3b31c
Roberth Daniel Oliveira Vargas <roliveirav@postgradounap.edu.pe>
2024-09-04 12:49:14 -0500

    feat: CardGroupd Component with props

--- src/app/(components)/card-group/page.tsx
+++ src/app/(components)/card-group/page.tsx
@@ -3,7 +3,13 @@ import { CardGroup } from "@/modules/core/components"
 export default function Page() {
     return (
         <div>
-            <CardGroup/>
+            <CardGroup
+                title="Proyecto X"
+                description="Descripción del proyecto X"
+                labelRef="Ver detalles"
+                progress={75}
+                states={true}
+            />
         </div>
     )
 }
