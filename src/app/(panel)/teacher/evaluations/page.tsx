34df9b8dfbb3d47ce91dda7a22abbbe5a41a2f9b
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-09-04 10:37:51 -0500

    feat: Add evaluations and profile pages for teacher panel
    This commit adds new components for the evaluations and profile pages in the teacher panel section. The evaluations page includes a header section with a title and subtitle to display and manage evaluations registered during different periods. The profile page includes a header section with a title and subtitle to register and update relevant user data.

---
+++ src/app/(panel)/teacher/evaluations/page.tsx
@@ -0,0 +1,13 @@
+import { HeaderSection } from '@/modules/core'
+
+export default function Page() {
+  return (
+    <main>
+      <HeaderSection
+        title="Evaluaciones"
+        subtitle="Registro de evaluaciones ya registradas durante diferentes periodos"
+        labelAddButton="Comenzar evaluación"
+      />
+    </main>
+  )
+}
