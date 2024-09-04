8ab4c6df71b68d2d3432d3f1e69179a79c12cfa6
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-09-04 07:46:17 -0500

    feat: Update HeaderSection component to conditionally show back button and add button

--- src/app/(components)/navbar/page.tsx
+++ src/app/(components)/navbar/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
           roles: options,
         }}
       />
-      <HeaderSection />
+      <HeaderSection showBackButton />
     </div>
   )
 }
--- src/modules/core/components/HeaderSection/HeaderSection.tsx
+++ src/modules/core/components/HeaderSection/HeaderSection.tsx
@@ -1,14 +1,22 @@
 import { Button } from '@/components/ui/button'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Plus } from 'lucide-react'
 
-export const HeaderSection = () => {
+interface IHeaderSectionProps {
+  showBackButton?: boolean
+  disableAddButton?: boolean
+}
+
+export const HeaderSection = (props: IHeaderSectionProps) => {
+  const { showBackButton, disableAddButton } = props
   return (
     <main className="py-6 border-b">
       <section className="container flex justify-between items-center">
         <div className="flex gap-3">
-          <Button className="py-1 px-2 bg-transparent border text-gray-500 hover:bg-gray-100">
-            <ArrowLeft size={20} />
-          </Button>
+          {showBackButton && (
+            <Button className="py-1 px-2 bg-transparent border text-gray-500 hover:bg-gray-100">
+              <ArrowLeft size={20} />
+            </Button>
+          )}
           <section>
             <h1 className="text-2xl font-medium">Header section</h1>
             <h2 className="text-sm text-gray-500">
@@ -18,7 +26,14 @@ export const HeaderSection = () => {
           </section>
         </div>
         <div>
-          <Button>Click me</Button>
+          {!disableAddButton && (
+            <Button>
+              <div className="flex gap-4">
+                <Plus size={20} />
+                <h3>Agregar</h3>
+              </div>
+            </Button>
+          )}
         </div>
       </section>
     </main>
