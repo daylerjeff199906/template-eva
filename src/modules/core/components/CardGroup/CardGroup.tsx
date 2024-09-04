a052840c25ba0ee664ccaba56e2d6e3f04d6c440
Roberth Daniel Oliveira Vargas <roliveirav@postgradounap.edu.pe>
2024-09-04 12:44:56 -0500

    feat: CardGroupd Component with props

--- src/modules/core/components/CardGroup/CardGroup.tsx
+++ src/modules/core/components/CardGroup/CardGroup.tsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-
-import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
@@ -10,9 +8,20 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Users } from "lucide-react"
+import Circleprogress from "./component/circleprogress"
+import Link from "next/dist/client/link"
+import { Button } from "@/components/ui/button"
+
+interface ICardGroupProps {
+    title: string,
+    description: string,
+    labelRef: string,
+    progress: number,
+    states: boolean,
+}
 
 
-export function CardGroup() {
+export function CardGroup({title = 'Title', description = 'Description', labelRef='Default', progress = 0, states=true} : ICardGroupProps) {
     return (
         <Card className="max-w-[350px]">
             <CardHeader>
@@ -22,21 +31,27 @@ export function CardGroup() {
                             <Users />
                         </div>
                         <div className="flex items-center justify-center border-2 rounded-md px-2 py-1 ">
-                            <h1 className="text-sm">Estado</h1>
+                            <h1 className="text-sm">{states ? 'En curso' : 'Terminado'}</h1>
                         </div>
                     </div>
                     <div className="flex">
-                        <h1>10%</h1>
-                        <div className="rounded-full bg-green-500 h-2 w-full">g</div>
+                        <Circleprogress value={progress} />
                     </div>
                 </div>
             </CardHeader>
             <CardContent>
-                <CardTitle><p className="line-clamp-1">Matematica - 5to A </p></CardTitle>
-                <CardDescription>Turno: Mañana</CardDescription>
+                <CardTitle><p className="line-clamp-1 mb-2">{title}</p></CardTitle>
+                <CardDescription>{description}</CardDescription>
             </CardContent>
             <CardFooter className="flex justify-between">
-                <Button>Deploy</Button>
+                <Button asChild>
+                    <Link
+                        href="/"
+                        className="bg-"
+                    >
+                        {labelRef}
+                    </Link>
+                </Button>
             </CardFooter>
         </Card>
     )
