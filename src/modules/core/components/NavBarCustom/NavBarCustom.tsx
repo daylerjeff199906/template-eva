c59632fe34fe8de67792b62b5a7def3e7adb0bdf
daylerjeff199906 <daylersan@gmail.com>
2024-09-03 20:29:51 -0500

    feat: fixed sections navbar

--- src/modules/core/components/NavBarCustom/NavBarCustom.tsx
+++ src/modules/core/components/NavBarCustom/NavBarCustom.tsx
@@ -1,17 +1,59 @@
 import Image from 'next/image'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+const options = [
+  { value: '1', label: 'Option 1' },
+  { value: '2', label: 'Option 2' },
+  { value: '3', label: 'Option 3' },
+]
 
 export const NavBarCustom = () => {
   return (
     <nav>
       <main className="container flex justify-between">
-        <section>
-          <div>
+        <section className="flex gap-4 items-center">
+          <div
+            id="section-brand"
+            className="w-full min-w-[140px]"
+          >
             <Image
               src="/images/logoGrel.webp"
               alt="logo"
               width={140}
               height={100}
             />
+          </div>
+          <hr className="h-10 w-0.5 bg-gray-300" />
+          <div
+            id="user-roles"
+            className="w-full max-w-xs min-w-[180px]"
+          >
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a fruit" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Roles</SelectLabel>
+                  {options.map((option) => (
+                    <SelectItem
+                      key={option.value}
+                      value={option.value}
+                    >
+                      {option.label}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
         </section>
         <section className="flex gap-6">
