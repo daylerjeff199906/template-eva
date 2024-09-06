6c785f0b2751a2a5a5f3039f527ed05186797f3b
daylerjeff199906 <daylersan@gmail.com>
2024-09-05 20:01:00 -0500

    feat: add header section in page evaluation

--- src/modules/teacher/page/EvaluacionRender/EvaluacionRender.tsx
+++ src/modules/teacher/page/EvaluacionRender/EvaluacionRender.tsx
@@ -14,7 +14,21 @@ export const EvaluacionRender = (props: IProps) => {
         subtitle="Turno: Mañana"
         disableAddButton
         showBackButton
+        renderLeftSection={<RenderLeftSection />}
       />
     </main>
+  )
+}
+
+const RenderLeftSection = () => {
+  return (
+    <section className="flex flex-col gap-1 border-l-2 px-4">
+      <h3 className="text-sm text-gray-500">
+        Institución Educativa: Nombre completo de colegio
+      </h3>
+      <h3 className="text-sm text-gray-500">
+        Director: Juan Piero Pancracio Herrera
+      </h3>
+    </section>
   )
 }
