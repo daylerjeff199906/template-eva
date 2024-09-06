ec9e30fd7ee434d5798f1c111770e2ebe08295ef
José Jefferson Santos Panaifo <jsantosp@postgradounap.edu.pe>
2024-09-06 09:03:51 -0500

    feat: optimize EvaluationTable component

--- src/modules/teacher/components/evaluationTable/EvaluationTable.tsx
+++ src/modules/teacher/components/evaluationTable/EvaluationTable.tsx
@@ -5,50 +5,12 @@ import 'react-data-grid/lib/styles.css'
 import DataGrid, {
   Column,
   ColumnOrColumnGroup,
-  // SelectCellFormatter,
   SelectColumn,
   SortColumn,
   textEditor,
 } from 'react-data-grid'
 
-// import type { Direction } from './types'
-// import type { Props } from './props'
-// import { textEditorClassname } from './TextEditor'
-
 import { faker } from '@faker-js/faker'
-// import { css } from '@linaria/core'
-
-// const toolbarClassname = css`
-//   display: flex;
-//   justify-content: flex-end;
-//   gap: 8px;
-//   margin-block-end: 8px;
-// `;
-
-// const dialogContainerClassname = css`
-//   position: absolute;
-//   inset: 0;
-//   display: flex;
-//   place-items: center;
-//   background: rgba(0, 0, 0, 0.1);
-
-//   > dialog {
-//     width: 300px;
-//     > input {
-//       width: 100%;
-//     }
-
-//     > menu {
-//       text-align: end;
-//     }
-//   }
-// `;
-
-// const dateFormatter = new Intl.DateTimeFormat(navigator.language)
-// const currencyFormatter = new Intl.NumberFormat(navigator.language, {
-//   style: 'currency',
-//   currency: 'eur',
-// })
 
 interface SummaryRow {
   id: string
@@ -139,8 +101,8 @@ function createRows(): readonly Row[] {
   for (let i = 0; i < 500; i++) {
     rows.push({
       id: i,
+      number_doc: Math.random().toString().slice(2, 10),
       student: faker.person.fullName() + ' ' + faker.person.lastName(),
-      number_doc: Math.random().toString(36).substring(7),
       progress: Math.random() * 100,
       progress2: Math.random() * 100,
       progress3: Math.random() * 100,
@@ -151,71 +113,25 @@ function createRows(): readonly Row[] {
   return rows
 }
 
-type Comparator = (a: Row, b: Row) => number
-
-function getComparator(sortColumn: string): Comparator {
-  switch (sortColumn) {
-    case 'id':
-      return (a, b) => {
-        return a.id - b.id
-      }
-    case 'student':
-    case 'number_doc':
-      return (a, b) => {
-        return a[sortColumn].localeCompare(b[sortColumn])
-      }
-    case 'progress':
-    case 'progress2':
-    case 'progress3':
-    case 'progress4':
-      return (a, b) => {
-        return a[sortColumn] - b[sortColumn]
-      }
-    default:
-      throw new Error(`unsupported sortColumn: "${sortColumn}"`)
-  }
-}
-
 export const EvaluationTable = () => {
   const [rows, setRows] = useState(createRows)
-  const [sortColumns, setSortColumns] = useState<readonly SortColumn[]>([])
   const [selectedRows, setSelectedRows] = useState(
     (): ReadonlySet<number> => new Set()
   )
 
   const columns = useMemo(() => getColumns(), [])
 
-  const sortedRows = useMemo((): readonly Row[] => {
-    if (sortColumns.length === 0) return rows
-
-    return [...rows].sort((a, b) => {
-      for (const sort of sortColumns) {
-        const comparator = getComparator(sort.columnKey)
-        const compResult = comparator(a, b)
-        if (compResult !== 0) {
-          return sort.direction === 'ASC' ? compResult : -compResult
-        }
-      }
-      return 0
-    })
-  }, [rows, sortColumns])
-
   const gridElement = (
     <DataGrid
       rowKeyGetter={rowKeyGetter}
       columns={columns as Column<Row>[]}
-      rows={sortedRows}
-      defaultColumnOptions={{
-        sortable: true,
-      }}
+      rows={rows}
       selectedRows={selectedRows}
       onSelectedRowsChange={setSelectedRows}
       onRowsChange={setRows}
-      sortColumns={sortColumns}
-      onSortColumnsChange={setSortColumns}
+      className="fill-grid"
       //   topSummaryRows={summaryRows}
       //   bottomSummaryRows={summaryRows}
-      className="fill-grid"
       //   direction={direction}
     />
   )
